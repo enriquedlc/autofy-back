7167233efa88e3e412e2fc8075bfbb2a61877638
masim0 <usr1693@salesianos-lacuesta.net>
2024-05-07 22:31:31 +0100

    :tada:

---
+++ src/main/java/com/autofy/autofyback/AutofyBackApplication.java
@@ -0,0 +1,13 @@
+package com.autofy.autofyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutofyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autofy/autofyback/AutofyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autofy.autofyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
